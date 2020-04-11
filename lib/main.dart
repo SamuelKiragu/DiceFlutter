439f6fa5bd275558cba37fe84b2d89eccee55d6a
Kiragu <samuelkiragu107@gmail.com>
2020-04-11 11:52:41 +0300

    made this dicee application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void pressed(){
+    setState(() {
+      rightDiceNumber = 1 + Random().nextInt(6);
+      leftDiceNumber = 1 + Random().nextInt(6);
+    });
+    print("right button has been pressed");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child:FlatButton(
+                onPressed: (){
+                  pressed();
+                },
+                child: Image.asset("images/dice$leftDiceNumber.png"),
+              )
+          ),
+          Expanded(
+              child: FlatButton(
+                onPressed: (){
+                  pressed();
+                },
+                child: Image.asset("images/dice$rightDiceNumber.png"),
+              )
+          )
+        ],
+      ),
+    );
   }
 }
+
